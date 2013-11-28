d5a7ddd157485027a8ea5ef52a61f141679195a6
andro_majid <andro.majid@redawning.com>
2013-11-28 14:31:25 +0700

    fix redirect problemsadd

--- themes/modern-business/views/layouts/main.php
+++ themes/modern-business/views/layouts/main.php
@@ -77,52 +77,52 @@ $baseUrl = Yii::app()->theme->baseUrl;
             </div><!-- /.container -->
         </nav>
 
-        <div id="myCarousel" class="carousel slide">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-                    <div class="carousel-caption">
-                        <h1>Modern Business - A Bootstrap 3 Template</h1>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                    <div class="carousel-caption">
-                        <h1>Ready to Style &amp; Add Content</h1>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                    <div class="carousel-caption">
-                        <h1>Additional Layout Options at <a href="http://startbootstrap.com">http://startbootstrap.com</a></h1>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="icon-prev"></span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="icon-next"></span>
-            </a>
-        </div>
+        <!--slide-->
         <?php
-        if (Yii::app()->user->hasFlash('error')) {
-            echo '<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">×</button>' . Yii::app()->user->getFlash('error') . '</div>';
-        }
-        if (Yii::app()->user->hasFlash('success')) {
-            echo '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button>' . Yii::app()->user->getFlash('success') . '</div>';
-        }
+        $this->widget('application.widget.widget_slide.widget_slide');
         ?>
+        <!--end slide-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <div class="section">
 
             <div class="container">
